bdfb80df11e529c9131ca9cfe953ede825b842ac
Aniruddha Bhandare <anirudhabhandare4303@gmail.com>
2025-08-26 00:49:53 +0530

    new change

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,30 @@
 import express from "express";
-const app = express();
-import contactRoute from "./routes/contact.js"
-import mongoose from "mongoose";
 import cors from "cors";
-import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import contactRoute from "./routes/contact.js";
+
 dotenv.config();
+
+const app = express();
 const PORT = process.env.PORT || 5000;
+
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+
 app.use("/api/contact", contactRoute);
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log(" MongoDB Connected"))
-  .catch((err) => console.error(" MongoDB Error:", err));
+
+
 app.get("/", (req, res) => {
   res.send("Backend is running...");
 });
+
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("MongoDB Error:", err));
+
 app.listen(PORT, () => {
-  console.log(` Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
